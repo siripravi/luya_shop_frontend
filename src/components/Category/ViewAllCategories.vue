df49566b071fcb6e3fa98587bec07c09dc491859
provdigi <purnachandra.valluri@gmail.com>
2024-07-01 10:48:42 +0530

    stle

--- src/components/Category/ViewAllCategories.vue
+++ src/components/Category/ViewAllCategories.vue
@@ -4,31 +4,31 @@
       <div class="container">
         <div class="row">
           <div class="categories__slider owl-carousel">
-          <template v-for="nodes in catData">
-            <template v-for="category in nodes">
-               
-                <div class="categories__item" v-if="category.slug !== undefined" :key="category.id">              
+            <template v-for="nodes in catData">
+              <template v-for="category in nodes">
+                <div
+                  class="categories__item"
+                  v-if="category.slug !== undefined"
+                  :key="category.id"
+                >
                   <a
-                class="text-black cursor-pointer hover:underline"
-                :href="`/category/${category.slug}`"
-              >
-              <div class="categories__item__icon">
-                    <span class="flaticon-030-cupcake-2"></span>
-                    <h5>{{ category.name }}</h5>
-                  </div>
-                
-                </a>
+                    class="text-black cursor-pointer hover:underline"
+                    :href="`/category/${category.slug}`"
+                  >
+                    <div class="categories__item__icon">
+                      <span class="flaticon-030-cupcake-2"></span>
+                      <h5>{{ category.name }}</h5>
+                    </div>
+                  </a>
                 </div>
-             
+              </template>
             </template>
-          </template>
-        </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 
 <script setup>
 defineProps(["catData"]);
